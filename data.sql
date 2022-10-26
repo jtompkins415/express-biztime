03c63166c084d22a3ef8cdc76d74d56eeaf9d86f
jtompkins415 <john.tompkins415@gmail.com>
2022-10-26 09:40:44 -0700

    create table for industries and companies-industries relation

--- data.sql
+++ data.sql
@@ -2,6 +2,8 @@
 
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS companies_industries;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -19,6 +21,18 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+
+CREATE TABLE industries (
+    code text PRIMARY KEY,
+    name text NOT NULL UNIQUE
+);
+
+CREATE TABLE companies_industries (
+  comp_code text REFERENCES companies ON DELETE CASCADE,
+  ind_code text REFERENCES industries ON DELETE CASCADE,
+  PRIMARY KEY (comp_code, ind_code)
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
@@ -28,3 +42,15 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries (code, name)
+  VALUES ("acct", "Accounting"),
+         ("rnd", "Research and Development"),
+         ("hr", "Human Resources"),
+         ("it", "Information Technology" );
+
+INSERT INTO companies_industries (comp_code, ind_code)
+  VALUES ("apple", "acct"),
+         ("apple", "hr"),
+         ("ibm", "it"),
+         ("ibm", "rnd");
